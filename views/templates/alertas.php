25778689273e3c81052c1a51c75be7793a441cf5
Alexanderel04 <2004.estrada.lopez@gmail.com>
2023-04-08 12:15:25 -0600

    Primer update

---
+++ views/templates/alertas.php
@@ -0,0 +1,13 @@
+<?php
+    foreach($alertas as $key => $mensajes):
+        foreach($mensajes as $mensaje):
+?>
+<div class="contenedor-alerta">
+    <div class="alerta <?php echo $key; ?>">
+            <?php echo $mensaje; ?>
+    </div>
+</div>
+<?php
+        endforeach;
+    endforeach;
+?>
